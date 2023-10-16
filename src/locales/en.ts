668f603b41d7f4b1929766fd6f367a95dadeef06
Aksenchenko Stefan <stefanaksencenko@gmail.com>
2023-10-16 06:23:52 +0600

    On branch main 	add locales

---
+++ src/locales/en.ts
@@ -0,0 +1,78 @@
+export default {
+  layout: {
+    nav: {
+      about: "about",
+      skills: "skills",
+      career: "career",
+      education: "education",
+      projects: "projects",
+    },
+    footer: {
+      copyright: "Created by Aksenchenko Stefan",
+    },
+  },
+  home: {
+    meta: {
+      title: "CV | Aksenchenko Stefan",
+      description:
+        "As a passionate and skilled frontend developer, I thrive on pushing the boundaries of technology. Explore my website to discover my portfolio and expertise in creating responsive and user-friendly web solutions.",
+    },
+    hero: {
+      position: "front-end developer",
+      name: "Aksenchenko Stefan",
+      download: "download cv",
+    },
+    about: {
+      title: "About me",
+      welcome: "Hello, I’m a Stefan!",
+      info: "I'm a senior front-end developer with a genuine passion for technology. I'm dedicated to self-improvement, and I make it a point to learn and grow every day. I strongly believe in the power of teamwork and foster a positive, toxicity-free work environment. Currently residing in Kazakhstan. I'm excited about the opportunity to apply my knowledge and experience to create innovative software solutions that can make a real impact.",
+      male: "male",
+      age: "27 y.o",
+      location: "Kazakhstan, Almaty",
+    },
+    skills: {
+      title: "technical skills",
+    },
+    career: {
+      title: "career history",
+      "Mygento eCommerce Solutions": "Mygento eCommerce Solutions",
+      "front-end developer": "front-end developer",
+      "As Front-end developer at Mygento eCommerce Solutions":
+        "As Front-end developer at Mygento eCommerce Solutions, I played an important role in supporting and enhancing software products. My responsibilities included:",
+      "Support and Development: I was responsible for actively participating in":
+        "Support and Development: I was responsible for actively participating in the development of new features and functionalities for our software products. This involved coding, testing, and implementing new solutions to meet project requirements.",
+      "Code Refactoring and Optimization: I consistently worked on improving the quality":
+        "Code Refactoring and Optimization: I consistently worked on improving the quality of codebases by conducting code refactoring and optimization. This process not only enhanced the performance of the software but also made it more maintainable and scalable.",
+      "Mentoring and Training: I took on the role of mentoring junior developers":
+        "Mentoring and Training: I took on the role of mentoring junior developers, providing guidance, and sharing best practices in software development. This helped the team grow and improve their skills, contributing to overall project success.",
+      "Client Communication: I established and maintained effective communication with clients":
+        "Client Communication: I established and maintained effective communication with clients to understand their needs and requirements. By actively engaging with clients, I ensured that our solutions aligned with their expectations and objectives.",
+      Nevafilm: "Nevafilm",
+      "back-end developer": "back-end developer",
+      "During my tenure at Nevafilm, I took on the role of developing a system designed to streamline the generation of documentation and contracts.":
+        "During my tenure at Nevafilm, I took on the role of developing a system designed to streamline the generation of documentation and contracts. My key responsibilities in this role included:",
+      "I led the design and development of a robust and user-friendly system for generating documentation and contracts.":
+        "System Development: I led the design and development of a robust and user-friendly system for generating documentation and contracts. This involved creating user interfaces, integrating databases, and implementing the necessary logic to automate the document generation process.",
+      "User-Friendly Interfaces: I ensured that the system's user interfaces were intuitive and easy to navigate":
+        "User-Friendly Interfaces: I ensured that the system's user interfaces were intuitive and easy to navigate, making it accessible to employees with varying levels of technical expertise.",
+      "Documentation Customization: I implemented features that allowed for customization of generated documents":
+        "Documentation Customization: I implemented features that allowed for customization of generated documents, tailoring them to specific client needs or project requirements.",
+    },
+    education: {
+      title: "education",
+      degree: "degree",
+      department: "department",
+      "Saint Petersburg State Chemical and Pharmaceutical University":
+        "Saint Petersburg State Chemical and Pharmaceutical University",
+      bachelor: "bachelor",
+      "chemical technology": "chemical technology",
+      master: "master (not completed)",
+      "economics and management of the Pharmaceutical industry":
+        "economics and management of the Pharmaceutical industry",
+    },
+    projects: {
+      title: "projects",
+      nda: "I've been involved in projects that are under the protection of NDA (Non-Disclosure Agreements).",
+    },
+  },
+} as const;
