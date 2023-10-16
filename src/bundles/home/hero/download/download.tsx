a9cc2bd50bde1aa469f0d7e86e7877787c2b6e96
Aksenchenko Stefan <stefanaksencenko@gmail.com>
2023-10-16 19:42:53 +0600

    On branch main 	Add cv

--- src/bundles/home/hero/download/download.tsx
+++ src/bundles/home/hero/download/download.tsx
@@ -4,14 +4,22 @@ import WithAppearance from "@/src/animations/appearance";
 import css from "./download.module.scss";
 import { ReactNode } from "react";
 import DownloadIcon from "@/src/components/icons/download";
+import { useCurrentLocale } from "@/src/locales/client";
 
 type DownloadProps = {
   children: ReactNode;
 };
 
+const cvLink = {
+  ru: "/cv/cv-aksenchenko stefan(ru).pdf",
+  en: "/cv/cv-aksenchenko stefan(en).pdf",
+};
+
 function Download({ children }: DownloadProps) {
+  const locale = useCurrentLocale();
+
   return (
-    <a href="#" className={css.download}>
+    <a href={cvLink[locale]} className={css.download}>
       {children}
       <DownloadIcon />
     </a>
